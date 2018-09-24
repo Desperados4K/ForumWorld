4d16507b6e14344ce878f40c342bcfb5438a2d60
wiolaSzczepanik <wiolaszcz@gmail.com>
2018-09-24 21:42:06 +0200

    add project

---
+++ forum_world/src/main/java/com/despedaros4k/forum_world/ForumWorldApplication.java
@@ -0,0 +1,12 @@
+package com.despedaros4k.forum_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumWorldApplication.class, args);
+	}
+}
---
+++ forum_world/src/test/java/com/despedaros4k/forum_world/ForumWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.despedaros4k.forum_world;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ForumWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
