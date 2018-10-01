38937692d89ea156951489568cd454566c22bab8
filipHartman <filip.hartman23@gmail.com>
2018-10-01 15:54:23 +0200

    refactor user entity

--- forum_world/src/main/java/com/despedaros4k/forum_world/entities/User.java
+++ forum_world/src/main/java/com/despedaros4k/forum_world/entities/User.java
@@ -2,33 +2,66 @@ package com.despedaros4k.forum_world.entities;
 
 import com.despedaros4k.forum_world.entities.enums.Gender;
 import com.despedaros4k.forum_world.entities.enums.Role;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.util.Collection;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class User {
+
+    public User(String userName, String firstName, String lastName, Gender gender, @NotEmpty(message = "Please provide an e-mail") @Email(message = "Please provide a valid e-mail") String email, Role role, String password, boolean authorized) {
+        this.userName = userName;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.email = email;
+        this.role = role;
+        this.password = password;
+        this.authorized = authorized;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(name = "user_name", nullable = false, unique = true)
     private String userName;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
+
+    @Column(name = "gender")
     @Enumerated(value = EnumType.STRING)
     private Gender gender;
+
+    @Column(name = "email", nullable = false, unique = true)
+    @NotEmpty(message = "Please provide an e-mail")
+    @Email(message = "Please provide a valid e-mail")
     private String email;
+
+    @Column(name = "role")
     @Enumerated(value = EnumType.STRING)
     private Role role;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "authorized")
     private boolean authorized;
+
+
     @OneToMany(
             fetch = FetchType.LAZY,
             mappedBy = "author",
@@ -40,6 +73,7 @@ public class User {
             }
     )
     private Collection<Topic> topics;
+
     @OneToMany(
             fetch = FetchType.LAZY,
             mappedBy = "author",
@@ -51,6 +85,7 @@ public class User {
             }
     )
     private Collection<Entry> entries;
+
     @OneToMany(
             fetch = FetchType.LAZY,
             mappedBy = "author",
