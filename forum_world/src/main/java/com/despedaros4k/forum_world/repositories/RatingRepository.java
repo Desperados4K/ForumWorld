cdec163cd78cc6e652f3b893229d2a5eb72b79ae
filipHartman <filip.hartman23@gmail.com>
2018-10-02 12:11:36 +0200

    add rating repository

---
+++ forum_world/src/main/java/com/despedaros4k/forum_world/repositories/RatingRepository.java
@@ -0,0 +1,7 @@
+package com.despedaros4k.forum_world.repositories;
+
+import com.despedaros4k.forum_world.entities.Rating;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RatingRepository extends CrudRepository<Rating, Long> {
+}
