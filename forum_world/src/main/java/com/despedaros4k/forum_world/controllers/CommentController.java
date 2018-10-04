fa4586ba982e5b33dad5b1efda43279a16c72f74
filipHartman <filip.hartman23@gmail.com>
2018-10-04 23:45:18 +0200

    add getting all comments from http request

--- forum_world/src/main/java/com/despedaros4k/forum_world/controllers/CommentController.java
+++ forum_world/src/main/java/com/despedaros4k/forum_world/controllers/CommentController.java
@@ -1,9 +1,11 @@
 package com.despedaros4k.forum_world.controllers;
 
 import com.despedaros4k.forum_world.entities.Comment;
+import com.despedaros4k.forum_world.services.CommentRestService;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,15 @@ public class CommentController {
 
     public static final String BASE_URL = "/api/v1/comments";
 
+    private final CommentRestService commentRestService;
+
+    public CommentController(CommentRestService commentRestService) {
+        this.commentRestService = commentRestService;
+    }
+
+    @GetMapping(produces = "application/hal+json")
     public ResponseEntity<Resources<Resource<Comment>>> allComments() {
-        return null;
+        return ResponseEntity.ok(commentRestService.findAll());
     }
 
     public ResponseEntity<Resource<Comment>> oneComment(Long id) {
