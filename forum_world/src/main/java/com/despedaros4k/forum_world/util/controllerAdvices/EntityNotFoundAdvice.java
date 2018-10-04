27512397e40f9e476209c459535eea0f998f0616
filipHartman <filip.hartman23@gmail.com>
2018-10-04 13:54:52 +0200

    add handler for bad request of not exsiting user

--- forum_world/src/main/java/com/despedaros4k/forum_world/util/controllerAdvices/EntityNotFoundAdvice.java
+++ forum_world/src/main/java/com/despedaros4k/forum_world/util/controllerAdvices/EntityNotFoundAdvice.java
@@ -1,4 +1,18 @@
 package com.despedaros4k.forum_world.util.controllerAdvices;
 
+import com.despedaros4k.forum_world.util.exceptions.UserNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
 public class EntityNotFoundAdvice {
+    @ResponseBody
+    @ExceptionHandler(UserNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String userNotFoundHandler(UserNotFoundException ex) {
+        return ex.getMessage();
+    }
 }
